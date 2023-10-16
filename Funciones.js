32ad1a8d19e0630a3b55a7c47af29749e70e75f7
sergior <sergio18.2002r@gmail.com>
2023-10-15 19:17:35 -0500

    15 agosto

--- Funciones.js
+++ Funciones.js
@@ -1,10 +1,21 @@
-function doGet()
-{
-    return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Registro Insumos');
+const HOJA = SpreadsheetApp.openById('1svdOstuMkQ60Fl6LdJPo65xqwy8NcJygzXSLEYgKQ1Q').getActiveSheet();
 
+function doGet(datos) {
+    return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Registro Insumos hemodinamia');
 }
 
-function obtenerDatosHTML(nombre)
-{
+function doPost(datos) {
+    return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Registro Insumos hemodinamia');
+}
+
+function obtenerDatosHTML(nombre) {
     return HtmlService.createHtmlOutputFromFile(nombre).getContent();
+}
+
+function obtenerInsumos() {
+    return HOJA.getDataRange().getValues();
+}
+
+function insertarInsumo(Referencia, Descripción, Precio2023, Proveedor, RegistroInvima) {
+    HOJA.appendRow([Referencia, Descripción, Precio2023, Proveedor, RegistroInvima]);
 }
